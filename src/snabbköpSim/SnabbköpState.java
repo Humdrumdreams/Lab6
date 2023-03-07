798fedf8311e4d533263be9479b01362a7f8784c
Botan Guzel <botanguzel@hotmail.com>
2023-03-07 15:53:56 +0100

    Update SnabbköpState.java

--- src/snabbköpSim/SnabbköpState.java
+++ src/snabbköpSim/SnabbköpState.java
@@ -1,39 +1,43 @@
-package snabbköpSim;
+package Lab6.snabbköpSim;
 
-import genSim.SimState;
+import Lab6.generellSim.SimState;
 
-public class SnabbköpState extends SimState{
+public class SnabbkopState extends SimState{
 	
-	private int tidAnkomst;
-	private int tidPlock;
-	private int tidBetalning;
-	private int antalKunderIButik;
+	private double tidAnkomst = -1.5;
+	private double tidPlock;
+	private double tidBetalning;
 	private int antalKunderSomHandlat;
 	private int antalKunderSomKöat;
 	private int summaTidKunderIKö;
 	private int kassaKöLängd;
-	private int kassaKöFIFO;
-	private int summaKöTid;
-	private int antalLedigaKassor;
+	private double summaKöTid;
 	private int summaTidLedigaKassor;
+	private int antalMissadeKunder;
+
+	private int antalKunderIButik;
+	private int kassaKöFIFO;
+	private int antalLedigaKassor;
+	private boolean snabbköpÖppet;
+	private int maxKunder;
 	
 	
-	public SnabbköpState() {//Ska man lägga in alla parameter värden här?
+	public SnabbkopState() {//Ska man lägga in alla parameter värden här?
 
 	}
 	
 	//Getters funktioner till alla privata instansvariabler.
-	public int getTidAnkomst() {
+	public double getTidAnkomst() {
 		return this.tidAnkomst;
 	}
-	public int getTidPlock() {
+	public void setTidAnkomst(double tid) {
+		this.tidAnkomst = tid;
+	}
+	public double getTidPlock() {
 		return this.tidPlock;
 	}
-	public int getTidBetalning() {
+	public double getTidBetalning() {
 		return this.tidBetalning;
-	}
-	public int getAntalKunderIButik() {
-		return this.antalKunderIButik;
 	}
 	public int getAntalKunderSomHandlat() {
 		return this.antalKunderSomHandlat;
@@ -47,19 +51,52 @@ public class SnabbköpState extends SimState{
 	public int getKassaKöLängd() {
 		return this.kassaKöLängd;
 	}
-	public int getKassaKöFIFO() {
-		return this.kassaKöFIFO;
-	}
-	public int getSummaKöTid() {
+	public double getSummaKöTid() {
 		return this.summaKöTid;
-	}
-	public int getAntalLedigaKassor() {
-		return this.antalLedigaKassor;
-	
 	}
 	public int getSummaTidLedigaKassor() {
 		return this.summaTidLedigaKassor;
 	}
 
+	public int getAntalMissadeKunder() {
+		return this.antalMissadeKunder;
+	}
 
+	public void setAntalMissadeKunder(int antalMissadeKunder) {
+		this.antalMissadeKunder = antalMissadeKunder;
+	}
+	
+	public int getAntalKunderIButik() {
+		return antalKunderIButik;
+	}
+	public void setAntalKunderIButik(int antalKunderIButik) {
+		this.antalKunderIButik = antalKunderIButik;
+	}
+	public void setKassaKöLängd(int kassaKöLängd) {
+		this.kassaKöLängd = kassaKöLängd;
+	}
+	public int getKassaKöFIFO() {
+		return kassaKöFIFO;
+	}
+	public void setKassaKöFIFO(int kassaKöFIFO) {
+		this.kassaKöFIFO = kassaKöFIFO;
+	}
+	public int getAntalLedigaKassor() {
+		return antalLedigaKassor;
+	}
+	public void setAntalLedigaKassor(int antalLedigaKassor) {
+		this.antalLedigaKassor = antalLedigaKassor;
+	}
+	public boolean isSnabbköpÖppet() {
+		return snabbköpÖppet;
+	}
+	public void setSnabbköpÖppet(boolean snabbköpÖppet) {
+		this.snabbköpÖppet = snabbköpÖppet;
+	}
+	public int getMaxKunder() {
+		return maxKunder;
+	}
+	public void setMaxKunder(int maxKunder) {
+		this.maxKunder = maxKunder;
+	}
 }
