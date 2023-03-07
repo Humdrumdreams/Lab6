16bde3077554abe0e3e83c023c10b4ae7e783289
Botan Guzel <botanguzel@hotmail.com>
2023-03-07 15:51:47 +0100

    Update Event.java

--- src/generellSim/Event.java
+++ src/generellSim/Event.java
@@ -1,8 +1,16 @@
-package generellSim;
+package Lab6.generellSim;
+
+import Lab6.snabbköpSim.SnabbkopState;
 
 public abstract class Event {
 	
-	public abstract void createEvent();
+	protected SnabbkopState state;
+	
+	public Event(SnabbkopState state) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public abstract void createEvent(double lambda, long seed);
 	//Klassen Event måste ha en referens till kön så dessa nya framtida
 	//händelser kan lagras med övriga framtida händelser.
 	
