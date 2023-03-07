6dbebff979eb6fa91882af9373dd86748c5de060
Botan Guzel <botanguzel@hotmail.com>
2023-03-07 15:53:33 +0100

    Update RunSim.java

--- src/mainSim/RunSim.java
+++ src/mainSim/RunSim.java
@@ -1,9 +1,26 @@
-package mainSim;
+package Lab6.mainSim;
+
+import Lab6.generellSim.Sim;
+import Lab6.snabbköpSim.Snabbkop;
+import Lab6.snabbköpSim.SnabbkopState;
+import Lab6.snabbköpSim.Kunder.Ankomst;
+import Lab6.snabbköpSim.Kunder.KundID;
 
 public class RunSim {
-
+	public static int maxAntal = 100;	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub //Ha  koll på allt i de båda paketen
+		KundID kID = new KundID();
+		kID.kundList.clear();
+		Snabbkop ss = new Snabbkop();
+		
+		ss.setSnabbköpÖppet(true);
+		ss.setAntalKunderIButik(1);
+		
+		SnabbkopState sss = new SnabbkopState();
+		Ankomst ankomst = new Ankomst(sss );
+		
+		ankomst.createEvent(12, 1234);
+		System.out.println("\t"+sss.getTidAnkomst());
 
 	}
 
