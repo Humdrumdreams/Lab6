da7e7638c3f4c8b3f038bcaa6a3f3bf82f4f3f41
Botan Guzel <botanguzel@hotmail.com>
2023-03-07 15:54:21 +0100

    Create Snabbkop.java

---
+++ src/snabbköpSim/Snabbkop.java
@@ -0,0 +1,12 @@
+package Lab6.snabbköpSim;
+
+public class Snabbkop {
+	private int antalKunderIButik;
+	private int kassaKöLängd;
+	private int kassaKöFIFO;
+	private int antalLedigaKassor;
+	private boolean snabbköpÖppet;
+	private int maxKunder;
+	
+	
+}
